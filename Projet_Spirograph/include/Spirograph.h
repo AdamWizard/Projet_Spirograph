d283994b840ae19f2cde43245f640e97ebbeeef7
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2021-09-27 10:48:30 +0200

    incroyable passage à codeblocks
    j'me fais flashbang à chaque ouverture tout va bien

---
+++ Projet_Spirograph/include/Spirograph.h
@@ -0,0 +1,16 @@
+#ifndef SPIROGRAPH_H
+#define SPIROGRAPH_H
+
+
+class Spirograph
+{
+    public:
+        Spirograph();
+        virtual ~Spirograph();
+
+    protected:
+
+    private:
+};
+
+#endif // SPIROGRAPH_H
---
+++ Projet_Spirograph/src/Spirograph.cpp
@@ -0,0 +1,11 @@
+#include "Spirograph.h"
+
+Spirograph::Spirograph()
+{
+    //ctor
+}
+
+Spirograph::~Spirograph()
+{
+    //dtor
+}
