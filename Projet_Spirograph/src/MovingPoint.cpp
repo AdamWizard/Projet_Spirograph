582904d00d134d14e63e9ab063bba0e3840ae72a
Florian Bethencourt <florian.bethencourt@etu.univ-tours.fr>
2021-10-12 15:08:11 +0200

    feat: We no longer use Point class, and Spirograph now initialise the MovingPoint list and link every point with their origin

--- Projet_Spirograph/src/MovingPoint.cpp
+++ Projet_Spirograph/src/MovingPoint.cpp
@@ -5,26 +5,57 @@ class MovingPoint
     public:
         MovingPoint()
         {
-            origin = new Point();
-            coord.setR(0); coord.setT(0);
+            origin = nullptr;
+            rho = 0; theta = 0; angSpeed = 0;
         }
 
         MovingPoint(const MovingPoint& copyPoint)
         {
             origin = copyPoint.origin;
-            coord.setR(copyPoint.getRho());
+            rho = copyPoint.getRho());
+            theta = copyPoint.setTheta());
+            angSpeed = copyPoint.getAngSpeed();
         }
 
-        MovingPoint(Point* origin0, const Point& coord0)
+        MovingPoint(MovingPoint* origin0, float rho0, float theta0, float angSpeed0)
         {
             origin = origin0;
-            coord.setRho(coord0.getRho());
-            coord.setTheta(coord0.setTheta());s
+            rho = rho0;
+            theta = theta0;
+            angSpeed = angSpeed0;
         }
 
         ~MovingPoint()
         {
             delete origin;
             origin = nullptr;
+        }
+
+        float MovingPoint::getRho() const
+        {
+            return rho;
+        }
+        float MovingPoint::getTheta() const
+        {
+            return theta;
+        }
+
+        float MovingPoint::getAngSpeed()
+        {
+            return angSpeed;
+        }
+
+        void MovingPoint::setRho(float newRho)
+        {
+            rho = newRho;
+        }
+
+        void MovingPoint::setTheta(float newTheta)
+        {
+            theta = newTheta;
+        }
+        void MovingPoint::setAngSpeed(float newAngSpeed)
+        {
+            angSpeed = newAngSpeed;
         }
 }
--- Projet_Spirograph/src/Spirograph.cpp
+++ Projet_Spirograph/src/Spirograph.cpp
@@ -1,22 +1,44 @@
-#include "Spirograph.h"
+#include "../include/Spirograph.h"
 #include <malloc.h>
 Spirograph::Spirograph()
 {
-    //initialiser avec un disque1 de taille 100 au centre et un disque 2 de taille 50 collé au premier (point a distance 150)
-    discs = (Disc**) malloc(sizeof(Disc*)*10);
-    Point* center1 = new Point(0,0); // Ca va changer avec les coordonnées polaires
-    Point* center2 = new Point(150, 0); // Ca va changer avec les coordonnées polaires
-    Disc* disc1= new Disc(center1, 100);
-    Disc* disc2= new Disc(center2, 50);
-    discs[0] = disc1; discs[1] = disc2;
+    int nbDisc;
+    cout << "How many discs do you want ? (enter a positive integer)" << endl;
+    do
+        cin >> nbDisc;
+    while(nbDisc<=0);
+
+    mPoints = (MovingPoint**)malloc(sizeof(MovingPoint*)*nbDisc);
+
+    mPoints[0] = new MovingPoint(nullptr, 0,0,0); //Point central immobile
+    first = mPoints[0];
+
+    float angSpeed = 0; float radius = 0;
+    for(int i = 1; i < nbDisc; i++)
+    {
+        cout << "What is the radius of the " << i << " th disc ? (enter  an integer > 0)" << endl;
+        do
+            cin >> radius;
+        while(radius<=0);
+
+        cout << "What is the angular speed of the " << i << " th disc in revolutions/min ? (enter  an integer > 0)" << endl;
+        do
+            cin >> angSpeed;
+        while(angSpeed<=0);
+
+        mPoints[i] = new MovingPoint(mPoints[i-1], radius, 0, angSpeed);
+    }
 }
 
 Spirograph::~Spirograph()
 {
-    while(*discs != nullptr)
+    while(*mPoints != nullptr)
     {
-        delete *discs;
-        discs++;
+        delete *mPoints;
+        mPoints++;
     }
-    delete this->discs;
+    delete this->mPoints;
+
+    delete first; delete last;
+    first = nullptr, last = nullptr;
 }
