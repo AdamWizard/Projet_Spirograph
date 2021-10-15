8b610f2e44549e440657d7417dd39676d65e99ef
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2021-10-15 16:57:22 +0200

    Update main.cpp

--- Projet_Spirograph/main.cpp
+++ Projet_Spirograph/main.cpp
@@ -7,11 +7,30 @@ using namespace std;
 
 int main()
 {
+    int nbDiscs;
+    cout << "How many discs will this spirograph have? ";
+    cin >> nbDiscs;
+
+    float d;
+    cout << "What will be the distance between the last discs center and the pencil?";
+    cin >> d;
+
+    Spirograph *spirograph = new Spirograph();
+
+    Disc* currentDisc = new Disc();
+    float radius;
+    //coordonnees centrees
+    for (int i=0;i<nbDiscs;i++){
+            cout << "radius of disc number " << i+1 << ": ";
+            cin >> radius ;
+        //ajouter un disque au spirograph aux coordonnees [somme des rayons des autres disques] en x
+    }
+
     //travail d'affichage du spirograph
 
     //mettre en place une fenetre et la possibilité d'intéragir
     sf::RenderWindow window(sf::VideoMode(640,640), "Spirograph",sf::Style::Titlebar | sf::Style::Close);
-    window.setFramerateLimit(120);
+    window.setFramerateLimit(60);
     sf::Event ev;
 
     //game loop
@@ -38,7 +57,7 @@ int main()
     movingCircle2.setOutlineColor(sf::Color::White);
     movingCircle2.setPosition(window.getSize().x/2+movingCircle.getRadius()+movingCircle2.getRadius(),window.getSize().y/2);
 
-    //float[2]* list;
+    float[2]* list;
     //FIN TEST
 
     float vx=0;
