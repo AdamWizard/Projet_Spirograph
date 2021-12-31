2529922639bb824212bd73e7c2b6e41f7b0e3b58
AdamWizard <48995051+AdamWizard@users.noreply.github.com>
2021-12-31 14:08:42 +0100

    Deleted Shape class
    shape class is useless right now, every specific shape will be simulated with a nulber of discs and segments (hopefully)

--- Projet_Spirograph/include/Segment.h
+++ Projet_Spirograph/include/Segment.h
@@ -1,16 +1,28 @@
 #ifndef SEGMENT_H
 #define SEGMENT_H
+#include "Point.h"
 
 
 class Segment
 {
     public:
-        Segment();
+        Segment(float startX,float startY,float endX,float endY);
+
         virtual ~Segment();
 
-    protected:
+        getStartX();
+
+        getStartY();
+
+        getEndX();
+
+        getEndY();
 
     private:
+        float startX;
+        float startY;
+        float endX;
+        float endY;
 };
 
 #endif // SEGMENT_H
--- Projet_Spirograph/src/Segment.cpp
+++ Projet_Spirograph/src/Segment.cpp
@@ -1,11 +1,15 @@
 #include "Segment.h"
+#include "SFML/Graphics.hpp"
 
-Segment::Segment()
+Segment::Segment(float startX,float startY,float endX,float endY)
 {
-    //ctor
+    this.startX=startX;
+    this.startY=startY;
+    this.endX=endX;
+    this.endY=endY;
 }
 
 Segment::~Segment()
 {
-    //dtor
+
 }
